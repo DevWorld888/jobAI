2bb2f0f5ce1ba9152c7b128438fdd467fbd877d3
Augusto Cs <augustocsuarez1985@gmail.com>
2025-05-02 21:35:23 +1000

    makin menu

--- src/app/layout/AppSidebar.tsx
+++ src/app/layout/AppSidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import {
   BoxCubeIcon,
   CalenderIcon,
@@ -14,44 +14,46 @@ import {
 } from "../../icons/index";
 import Link from 'next/link';
 import { useSidebar } from '@/context/SidebarContext';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 type NavItem = {
   name: string,
-  icon: React.ReactNode,
+  icon: React.ComponentType<any>,
   path?: string,
-  subItems?: (Omit<NavItem, 'icon'>)[],
+  subItems?: { name: string; path: string; pro?: boolean; new?: boolean }[];
 }
 
 const navItems: NavItem[] = [
   {
-    icon: <GridIcon />,
+    icon: GridIcon ,
     name: "Dashboard",
     subItems: [{ name: "Ecommerce", path: "/", pro: false }],
   },
   {
-    icon: <CalenderIcon />,
+    icon: CalenderIcon ,
     name: "Calendar",
     path: "/calendar",
   },
   {
-    icon: <UserCircleIcon />,
+    icon: UserCircleIcon ,
     name: "User Profile",
     path: "/profile",
   },
 
   {
     name: "Forms",
-    icon: <ListIcon />,
+    icon: ListIcon ,
     subItems: [{ name: "Form Elements", path: "/form-elements", pro: false }],
   },
   {
     name: "Tables",
-    icon: <TableIcon />,
+    icon: TableIcon ,
     subItems: [{ name: "Basic Tables", path: "/basic-tables", pro: false }],
   },
   {
     name: "Pages",
-    icon: <PageIcon />,
+    icon: PageIcon ,
     subItems: [
       { name: "Blank Page", path: "/blank", pro: false },
       { name: "404 Error", path: "/error-404", pro: false },
@@ -61,7 +63,7 @@ const navItems: NavItem[] = [
 
 const othersItems: NavItem[] = [
   {
-    icon: <PieChartIcon />,
+    icon: PieChartIcon ,
     name: "Charts",
     subItems: [
       { name: "Line Chart", path: "/line-chart", pro: false },
@@ -69,7 +71,7 @@ const othersItems: NavItem[] = [
     ],
   },
   {
-    icon: <BoxCubeIcon />,
+    icon: BoxCubeIcon ,
     name: "UI Elements",
     subItems: [
       { name: "Alerts", path: "/alerts", pro: false },
@@ -81,7 +83,7 @@ const othersItems: NavItem[] = [
     ],
   },
   {
-    icon: <PlugInIcon />,
+    icon: PlugInIcon ,
     name: "Authentication",
     subItems: [
       { name: "Sign In", path: "/signin", pro: false },
@@ -92,21 +94,89 @@ const othersItems: NavItem[] = [
 
 const AppSidebar: React.FC = () => {
   const { isExpanded, isMobileOpen, isHovered, setIsHovered } = useSidebar();
+  const pathname = usePathname();
+  const [openSubmenu, setOpenSubmenu] = useState<{
+    type: "main" | "others";
+    index: number;
+  } | null>(null);
 
-  const renderMenuItems = (items: NavItem[], menutype: "main" | "others") => {
+  const renderMenuItems = (
+    items: NavItem[],
+    menuType: "main" | "others"
+  ) => {
     return (
       <>
-         <ul className="flex flex-col gap-4 text-amber-50">
-            {items.map((nav,index) =>(
-              <li key={nav.name}>
-                {nav.name}
-              </li>
-             ))}
-         </ul>
+        <ul className="flex flex-col gap-4 text-amber-50">
+          {items.map((nav, index) => {
+            const Icon = nav.icon;
+            return(
+            <li key={nav.name}>
+              {nav.subItems ? (
+                <button onClick={() => handleSubmenuToggle(index, menuType)}
+                  className={`menu-item group  ${openSubmenu?.type === menuType && openSubmenu?.index === index
+                    ? "menu-item-active"
+                    : "menu-item-inactive"
+                    } cursor-pointer ${!isExpanded && !isHovered
+                      ? "lg:justify-center"
+                      : "lg:justify-start"
+                    }`}
+                >
+                  <span
+                    className={` ${openSubmenu?.type === menuType && openSubmenu?.index === index
+                      ? "menu-item-icon-active"
+                      : "menu-item-icon-inactive"
+                      }`}
+                  >
+                     <Icon className="w-5 h-5" />
+                  </span>
+                  {(isExpanded || isHovered || isMobileOpen) && (
+                    <span className={`menu-item-text`}>{nav.name}</span>
+                  )}
+                </button>
+              ) : (
+                nav.path && (
+                  <Link
+                href={nav.path}
+                className={`menu-item group ${
+                  isActive(nav.path) ? "menu-item-active" : "menu-item-inactive"
+                }`}
+              >
+                {/* <span
+                  className={`${
+                    isActive(nav.path)
+                      ? "menu-item-icon-active"
+                      : "menu-item-icon-inactive"
+                  }`}
+                >
+                   <Icon className="w-5 h-5" />
+                </span> */}
+                {(isExpanded || isHovered || isMobileOpen) && (
+                  <span className={`menu-item-text`}>{nav.name}</span>
+                )}
+              </Link>
+                )
+              )}
+            </li>
+            )
+        })}
+        </ul>
       </>
     )
   };
 
+  const isActive = useCallback((path: string) => path === pathname, [pathname]);
+  const handleSubmenuToggle = (index: number, menuType: "main" | "others") => {
+    setOpenSubmenu((prevOpenSubmenu) => {
+      if (
+        prevOpenSubmenu &&
+        prevOpenSubmenu.type === menuType &&
+        prevOpenSubmenu.index === index
+      ) {
+        return null;
+      }
+      return { type: menuType, index };
+    });
+  };
   return (
     <aside className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200   lg:translate-y-0 lg:shadow-none shadow-lg
       ${isExpanded || isMobileOpen
@@ -120,10 +190,38 @@ const AppSidebar: React.FC = () => {
       onMouseEnter={() => !isExpanded && setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
+      <div className={`py-8 flex  ${!isExpanded && !isHovered ? "lg:justify-center" : "justify-start"
+        }`}>
+        {isExpanded || isHovered || isMobileOpen ? (
+          <>
+            <Image
+              className="dark:hidden"
+              src="/images/logo/logo.svg"
+              alt="Logo"
+              width={150}
+              height={40}
+            />
+            <Image
+              className="hidden dark:block"
+              src="/images/logo/logo-dark.svg"
+              alt="Logo"
+              width={150}
+              height={40}
+            />
+          </>
+        ) : (
+          <Image
+            src="/images/logo/logo-icon.svg"
+            alt="Logo"
+            width={32}
+            height={32}
+          />
+        )}
+      </div>
       <div className="flex flex-col overflow-y-auto duration-300 ease-linear no-scrollbar">
         <nav className="mb-6">
           <ul className="space-y-2">
-            {renderMenuItems(navItems, "nav")}
+            {renderMenuItems(navItems, "main")}
           </ul>
         </nav>
       </div>
